69c80a5bd4d97d74ea36ebb9e5615a778f0e1042
Jonatan <jonatanav255@hotmail.com>
2024-07-22 10:21:39 -0600

    Solution
    Time complexity: O(n)
    Space complexity: O(n)

---
+++ LeetCode/226/LeetCode226.js
@@ -0,0 +1,27 @@
+// 226. Invert Binary Tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (root === null) return null
+  let temp = root.left
+  root.left = root.right
+  root.right = temp
+  invertTree(root.left)
+  invertTree(root.right)
+
+  return root
+}
