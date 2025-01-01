6352eb74795cbadca2ba35651beba870f48dd5cd
Jonatan <jonatanav255@hotmail.com>
2025-01-01 12:46:18 -0600

    adding is empty

--- LeetCode/StackImplementation/StackImplementation.js
+++ LeetCode/StackImplementation/StackImplementation.js
@@ -1,11 +1,18 @@
 class Stack {
   constructor () {
-    this.items = []
+    this.items = [] // Array to store stack elements
   }
 
+  // Check if the stack is empty (custom method)
+  isEmpty () {
+    return this.items.length === 0
+  }
+
+  // Push an element onto the stack
   push (element) {
     this.items.push(element)
   }
+
 }
 
 const stack = new Stack()
