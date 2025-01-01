6610d59c0e0da53a0980b744032fcdcf83458f7c
Jonatan <jonatanav255@hotmail.com>
2025-01-01 13:08:07 -0600

    adding pop()

--- LeetCode/StackImplementation/StackImplementation.js
+++ LeetCode/StackImplementation/StackImplementation.js
@@ -12,17 +12,31 @@ class Stack {
   push (element) {
     this.items.push(element)
   }
+
+  // Pop (remove and return the top element of the stack)
+  pop () {
+    // First check if the stack is empty
+    if (this.isEmpty()) {
+      throw new Error('Stack is empty') // Prevent popping from an empty stack
+    }
+
+    // Use the native `pop` method to remove and return the last element
+    return this.items.pop()
+  }
 }
 
 const stack = new Stack()
 console.log('--------- Stack --------')
-console.log('Is stack empty? ', stack.isEmpty()) // Output: false
 
 stack.push(1)
 stack.push(2)
 stack.push(3)
 
-console.log('Is stack empty? ', stack.isEmpty()) // Output: false
+console.log('Stack:')
+console.log(stack.items)
+
+console.log('Popped:', stack.pop())
+console.log('Popped:', stack.pop())
 
 console.log('Stack:')
 console.log(stack.items)
